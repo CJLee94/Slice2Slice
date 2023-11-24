e83d77cf1df384166f44b8582b184981ab232662
CJLee94 <sl5954@nyu.edu>
2023-11-23 23:16:46 -0500

    add function to calculate ssim and psnr

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import pickle
 import PIL.Image
+import cv2
 
 # import dnnlib.submission.submit as submit
 
@@ -13,3 +14,56 @@ def save_pkl(obj, filename):
 def load_pkl(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
+    
+
+def ssim(prediction, target):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+    img1 = prediction.astype(np.float64)
+    img2 = target.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+    ssim_map = ((2 * mu1_mu2 + C1) *
+                (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                       (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+
+def calculate_ssim(target, ref):
+    '''
+    calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    img1 = np.array(target, dtype=np.float64)
+    img2 = np.array(ref, dtype=np.float64)
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1[:, :, i], img2[:, :, i]))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+
+
+def calculate_psnr(target, ref):
+    img1 = np.array(target, dtype=np.float32)
+    img2 = np.array(ref, dtype=np.float32)
+    diff = img1 - img2
+    psnr = 10.0 * np.log10(255.0 * 255.0 / np.mean(np.square(diff)))
+    return psnr
